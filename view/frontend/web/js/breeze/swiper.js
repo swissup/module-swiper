a48098402bb491256b68d24c4984d0ec403b45d0
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2021-05-13 10:47:00 +0300

    Breeze: swiper-wrapper implementation

--- view/frontend/web/js/breeze/swiper.js
+++ view/frontend/web/js/breeze/swiper.js
@@ -2,9 +2,36 @@
 (function () {
     'use strict';
 
+    var navigationHtml = '<div class="swiper-button-prev swiper-button-black"></div>' +
+                         '<div class="swiper-button-next swiper-button-black"></div>';
+
     breeze.widget('swiper', {
         /** [create description] */
         create: function () {
+            var target, container;
+
+            // swiper-wrapper implementation
+            if (this.options.target) {
+                target = this.options.target.container ?
+                    this.options.target.container : this.options.target;
+
+                container = $(target, this.element).addClass('swiper-container');
+                container.children(this.options.target.wrapper)
+                    .addClass('swiper-wrapper')
+                    .children(this.options.target.slide)
+                    .addClass('swiper-slide');
+
+                container.append(navigationHtml);
+
+                this.element = container;
+                this.options = $.extend({
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev'
+                    }
+                }, this.options);
+            }
+
             // Adjust swiper size when slide is loaded lazy.
             if (this.options.lazy) {
                 this.options.on = $.extend({}, this.options.on, {
